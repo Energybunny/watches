2324d406794ec44a5096fe67edf8b958dab69ec9
Energybunny <wilsson.danne@gmail.com>
2019-04-12 19:55:44 +0200

    Commit 1

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Denna kodens syfte är menyn som man kan öppna på min navbar. När man klickar
+på ikonen så ska menyn öppnas. När man klickar igen så stängs den */
+function toggleNavbar() {
+	document.getElementById("dropmenu").classList.toggle("extended");
+}
+
+
